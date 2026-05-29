b1adc1dd61bd81340b3ad46710feb635fc7b5db6
joshitha1808 <joshitha0818@gmail.com>
2026-05-29 22:23:23 +0530

    refactor: replace MyApp with MainApp and integrate theme management

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'core/theme/theme_provider.dart';
 
 void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+  runApp(const ProviderScope(child: MainApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class MainApp extends ConsumerWidget {
+  const MainApp({super.key});
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(themeProvider);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SmartVitals',
+      theme: theme,
       home: const Scaffold(
         body: Center(
           child: Text(
             'SmartVitals',
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
           ),
         ),
       ),
