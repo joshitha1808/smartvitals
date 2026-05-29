29aac3dabf1fd2701544e039633724ad324098d2
joshitha1808 <joshitha0818@gmail.com>
2026-05-29 23:14:53 +0530

    feat: update scanDevices method to return Either type for error handling

--- lib/features/ble/data/services/ble_service.dart
+++ lib/features/ble/data/services/ble_service.dart
@@ -1,15 +1,15 @@
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:fpdart/fpdart.dart';
 
 class BleService {
-  // BLE controller object
   final FlutterReactiveBle _ble = FlutterReactiveBle();
-  // Scan nearby BLE devices
-  Stream<DiscoveredDevice> scanDevices() {
-    return _ble.scanForDevices(
-      // Empty means scan all BLE devices
-      withServices: [],
-      // Fast scanning mode
-      scanMode: ScanMode.lowLatency,
-    );
+  Stream<Either<String, DiscoveredDevice>> scanDevices() async* {
+    try {
+      yield* _ble
+          .scanForDevices(withServices: [], scanMode: ScanMode.lowLatency)
+          .map((device) => right(device));
+    } catch (e) {
+      yield left('Failed to scan BLE devices');
+    }
   }
 }
